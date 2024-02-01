e27effdb71cbf9b5803ffb14053f0af8ef202a0d
karpov2713 <karpov2713@gmail.com>
2024-02-01 21:07:27 +0300

    Добавил минианкету из курса

--- CS and CLR/Program.cs
+++ CS and CLR/Program.cs
@@ -16,7 +16,32 @@ namespace CS_and_CLR
 
             const string myName = "Dima";
             Console.WriteLine(myName);
-            Console.ReadKey();
+
+            Console.WriteLine("\u0023"); // Символ # Unicode
+
+            // Логические литералы
+            Console.WriteLine("Логические литералы");
+            Console.WriteLine(true);
+            Console.WriteLine(false);
+
+            // Числовые литералы
+            Console.WriteLine("Числовые литералы");
+            Console.WriteLine(5);
+
+            // Небольшая анкета
+            string jName = "Jane";
+            byte age = 27;
+            bool HaveIApet = true;
+            double MyShoeSize = 37.5;
+
+            Console.WriteLine("My name is " + jName);
+            Console.WriteLine("My age is " + age);
+            Console.WriteLine("Do I have a pet? " + HaveIApet);
+            Console.WriteLine("My shoe size is " + MyShoeSize);
+
+
+
+
         }
     }
 }
