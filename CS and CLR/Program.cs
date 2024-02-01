8417eeb68e4d108fe648351e1183d6361cb60b8f
karpov2713 <karpov2713@gmail.com>
2024-02-01 19:48:01 +0300

    Добавьте файлы проекта.

---
+++ CS and CLR/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_and_CLR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Dmitry";
+            Console.WriteLine(MyName);
+            // Console.ReadKey();
+        }
+    }
+}
